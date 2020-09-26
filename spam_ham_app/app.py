d5bd7dbcca362f1e340bdb852700a47dc0652457
Sanchita Aggarwal <51820171+sanchita1306@users.noreply.github.com>
2020-09-26 23:00:06 +0530

    Add files via upload

---
+++ spam_ham_app/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import string
+import pickle
+import nltk
+import pandas
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+
+app = Flask(__name__)
+
+def text_process(mess):
+    nopunc =[char for char in mess if char not in string.punctuation]
+    nopunc=''.join(nopunc)
+    return [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
+
+@app.route('/')
+def hello():
+    return render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    msg = request.form['msg']
+    with open('model.pickle', 'rb') as f:
+        model = pickle.load(f)
+    prediction = model.predict_proba([msg])
+    result = list(prediction[0]).index(max(list(prediction[0])))
+    print("----------------->>>>>>>>>>>",result)
+    if result == 0:
+        var = '1'
+    else:
+        var = '0'
+    print("***************************",var)
+    return render_template("index.html", var=var)
+
+if __name__ == '__main__':
+    app.run(debug=True)
